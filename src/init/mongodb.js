105b6a6acb14a675a3048652d004f1b4f81b6333
周鹏 <zhoupeng@xiaomi.com>
2016-04-12 17:46:22 +0800

    添加mongodb连接

---
+++ src/init/mongodb.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+module.exports = function(done){
+    const conn = mongoose.createConnection($.config.get('db.mongodb'));
+}
---
+++ src/server.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import path from 'path';
+import ProjectCore from 'project-core';
+
+const $ = global.$ = new ProjectCore();
+
+$.init.add((done) => {
+    $.config.load(path.resolve(__dirname, 'config.js'));
+    const env = process.env.NODE_ENV.trim() || null;
+    if(env){
+        $.config.load(path.resolve(__dirname, '../config', env + '.js'));
+    }
+    $.env = env;
+    done();
+});
+
+$.init.load(path.resolve(__dirname, 'init', 'mongodb.js'))
+
+$.init((err) => {
+    if (err){
+        console.error(err);
+        process.exit(-1);
+    } else {
+        console.log('inited [env=%s]',$.env);
+    }
+})
