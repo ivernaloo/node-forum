63b19960766dbf59b677fbba4700f3ce8e828b67
周鹏 <zhoupeng@xiaomi.com>
2016-04-14 16:12:30 +0800

    加入了测试的路由

--- src/config.js
+++ src/config.js
@@ -1,3 +1,23 @@
-module.exports = function(set, get, has){
+'use strict';
 
-}
+/**
+ * pratice Node.js project
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+module.exports = function (set, get, has) {
+
+    // 服务器监听端口
+    set('web.port', 3000);
+
+    // session secret
+    // set('web.session.secret', 'test');
+
+    // session redis connection
+    // set('web.session.redis', {
+    //     host: '192.168.99.100',
+    //     port: 32768
+    // });
+
+};
--- src/init/express.js
+++ src/init/express.js
@@ -1,6 +1,25 @@
 "use strict";
 
 
-module.exports = function(){
+import path from 'path';
+import express from 'express';
+import serveStatic from 'serve-static';
+import bodyParser from 'body-parser';
 
+module.exports = function(done){
+
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+
+    const router = express.Router();
+    $.router = router;
+
+    app.use(router);
+    app.use('/static', serveStatic(path.resolve(__dirname, '../../static')));
+
+    app.listen($.config.get('web.port'), (err)=> {
+        done(err);
+    });
 };
