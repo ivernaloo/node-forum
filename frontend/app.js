6309192fad0cd1ac7a781b82763d0666c45e9175
aloo <aloocs@gmail.com>
2016-04-26 18:55:03 +0800

    体验了一下惊人的react以及hot

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>~~1231231312312312313~~</h1>
+            </div>
+        );
+    }
+}
