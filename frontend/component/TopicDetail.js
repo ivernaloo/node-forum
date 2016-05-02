d72b168793576db75cade9e2cfc4f9f837c8e3e5
aloo <aloocs@gmail.com>
2016-05-02 21:51:56 +0800

    完善了topicdetail的读取方法

--- frontend/component/TopicDetail.js
+++ frontend/component/TopicDetail.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {getTopicDetail} from '../lib/client';
 
 export default class TopicDetail extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        getTopicDetail(this.props.params.id);
+            .then(topic => this.setState({topic}))
+            .catch(err => console.error(err));
+    }
+
     render() {
+        const topic = this.state.topic;
+        if(!topic){
+            return (
+                <div>正在加载...</div>
+            )
+        }
         return (
             <div>
-                <h2>我是一个TopicDetail</h2>
+                <h2>{topic.title}</h2>
+                <section>{topic.content}</section>
             </div>
         )
 
