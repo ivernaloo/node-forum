61f8e2a042905dc6e518e3c76e334491d8b0f3d9
aloo <aloocs@gmail.com>
2016-05-03 22:12:06 +0800

    调整了route

--- frontend/app.js
+++ frontend/app.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import {Router, Route, Link, browserHistory} from 'react-router'
+
 import Header from './component/Header';
 import Footer from './component/Footer';
-import TopicDetail from './component/TopicDetail';
+
 import TopicList from './component/TopicList';
 
-class Index extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <div className="container">
                 <Header/>
-                    {this.props.children ? this.props.children : <TopicList />}
+                {this.props.children ? this.props.children : <TopicList />}
                 <Footer/>
             </div>
-        )
-    }
-}
-export default class App extends React.Component {
-    render() {
-        return (
-                <Router history={browserHistory}>
-                    <Route path="/" component="{Index}">
-                        <Route path="/topic/:id" component={TopicDetail}/>
-                    </Route>
-                </Router>
         );
     }
 }
--- frontend/entry.js
+++ frontend/entry.js
@@ -1,8 +1,9 @@
 import 'bootstrap-webpack';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link, browserHistory} from 'react-router'
 import App from './App';
-
+import TopicDetail from './component/TopicDetail';
 
 const e = document.createElement('div');
 e.className = 'container';
@@ -12,4 +13,10 @@ document.body.appendChild(e);
 
 console.log("right way");
 
-ReactDOM.render(<App />, e);
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="/topic/:id" component={TopicDetail}/>
+        </Route>
+    </Router>   
+), e);
