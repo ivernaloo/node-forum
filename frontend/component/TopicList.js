8f46f5cb4d8ab352f2785e3d79e5056a181af9dc
aloo <aloocs@gmail.com>
2016-05-16 17:50:11 +0800

    翻页的功能

--- frontend/component/TopicList.js
+++ frontend/component/TopicList.js
@@ -10,13 +10,39 @@ export default class TopicList extends React.Component {
     }
 
     componentDidMount() {
-        getTopicList({})
-            .then(ret => this.setState({list: ret.list}))
+        console.log("componentDidMount");
+        this.updateList({
+            tags: this.props.location.query.tags,
+            page: this.props.location.query.page
+        });
+    }
+
+    componentWillReceiveProps(nextPros){
+        console.log('componentWillReceiveProps');
+        this.updateList({
+            tags: this.props.location.query.tags,
+            page: this.props.location.query.page
+        });
+    }
+
+    updateList(query) {
+        getTopicList(query)
+            .then(ret => this.setState(ret))
             .catch(err => console.error(err));
     }
 
     render() {
+
         const list = Array.isArray(this.state.list) ? this.state.list : [];
+
+        let page = parseInt(this.state.page, 10);
+        if (!(page > 1)) page = 1;
+
+        let prevPage = page - 1;
+        if (prevPage < 1) prevPage = 1;
+
+        let nextPage = page + 1;
+
         return (
             <div>
                 <ul className="list-group">
@@ -26,6 +52,20 @@ export default class TopicList extends React.Component {
                         )
                     })}
                 </ul>
+                <nav>
+                    <ul className="pagination">
+                        <li>
+                            <Link to={`/?page=${prevPage}`} aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={`/?page=${nextPage}`} aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
