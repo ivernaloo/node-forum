dcd56aa6de2f2fd0d42efdd233676a91d7420ba2
aloo <aloocs@gmail.com>
2016-05-18 21:16:20 +0800

    评论删除的逻辑

--- src/routes/topic.js
+++ src/routes/topic.js
@@ -93,17 +93,29 @@ module.exports = function (done) {
         res.apiSuccess({comment});
     })
 
-    $.router.post('/api/topic/item/:topic_id/comment/delete',$.checkLogin, $.checkTopicAuthor, async function(req, res, next){
+    $.router.post('/api/topic/item/:topic_id/comment/delete',$.checkLogin, async function(req, res, next){
 
         req.body._id = req.params.topic_id;
-        req.body.author = req.session.user._id;
-        const comment = await $.method('topic.comment.delete').call({
+
+        const query = {
             _id: req.params.topic_id,
             cid: req.body.cid
-        });
-        // const comment = await $.method('topic.delete').call(req.body);
+        };
 
-        res.apiSuccess({comment});
+        const comment = await $.method('topic.comment.get').call(query);
+
+        if (comment && comment.comments && comment.comments[0]) {
+            const item = comment.comments[0];
+            if (req.session.user.isAdmin || item.author.toString() === req.session.user._id.toSting()) {
+                await  $.method('topic.comment.delete').call(query);
+            } else {
+                return next(new Error('access denied'));
+            }
+        }else {
+                return next(new Error('comment does not exists'));
+        }
+
+        res.apiSuccess({comment: comment.comments[0]});
     })
     
     done();
