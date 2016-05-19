0522c8ad445004ec335da97b6c07ca94429fd1e0
aloo <aloocs@gmail.com>
2016-05-19 21:52:33 +0800

    加入notification count

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -3,13 +3,30 @@
 
 module.exports = function(done) {
 
-    $.router.post('/api/notification/count', $.checkLogin, async function(req, res, next){
+    function formatQuery(ret, query){
+        if (query.type) ret.type = query.type;
+        if ('isRead' in query) ret.isRead = parseBoolean(query.isRead);
+        return ret;
+    }
 
-        req.body.quthor = req.session.user._id;
+    function parseBoolean(v){
+        switch (String(v)){
+            case 'true':
+            case '1':
+            case 'on':
+                return true;
+            default:
+                return false;
+        }
+    }
 
+    $.router.get('/api/notification/count', $.checkLogin, async function(req, res, next){
+
+        const query = formatQuery({to: req.session.user._id}, req.query);
+        console.log(query);
         const count = await $.method('notification.count').call(req.query);
 
-        res.apiSuccess({topic});
+        res.apiSuccess({count});
     });
 
     done();
