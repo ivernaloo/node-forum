5c52c170b12b1aec1ecedb89c86988d1d1d625c6
aloo <aloocs@gmail.com>
2016-05-19 22:17:44 +0800

    拿到通知列表

--- src/routes/notification.js
+++ src/routes/notification.js
@@ -4,7 +4,7 @@
 module.exports = function(done) {
 
     function formatQuery(ret, query){
-        if (query.type) ret.type = query.type;
+        if (query.type) ret.type = query.type; //加入查寻的type
         if ('isRead' in query) ret.isRead = parseBoolean(query.isRead);
         return ret;
     }
@@ -22,11 +22,24 @@ module.exports = function(done) {
 
     $.router.get('/api/notification/count', $.checkLogin, async function(req, res, next){
 
-        const query = formatQuery({to: req.session.user._id}, req.query);
+        const query = formatQuery({to: req.session.user._id}, req.query); // 拿到query的字符串
         console.log(query);
         const count = await $.method('notification.count').call(req.query);
 
         res.apiSuccess({count});
+    });
+
+    $.router.get('/api/notification/list', $.checkLogin, async function(req, res, next){
+
+        const query = formatQuery({to: req.session.user._id}, req.query); // 拿到query的字符串
+        const count = await $.method('notification.count').call(req.query);
+
+        if (req.query.skip) query.skip = req.query.skip;
+        if (req.query.limit) query.limit = req.query.limit;
+
+        const list = await $.method('notification.list').call(query);
+
+        res.apiSuccess({count, list});
     });
 
     done();
