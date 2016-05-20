657c6626f15fa5dcf09133a85f9dafea742916c7
aloo <aloocs@gmail.com>
2016-05-20 22:09:20 +0800

    加入了limiter，限制快速发帖

--- src/init/limiter.js
+++ src/init/limiter.js
@@ -11,8 +11,12 @@ module.exports = function(done){
     const prefix = $.config.get('limiter.redis.prefix');
     $.limiter = {connection}; // 全局记录
 
+    /*
+    * key {string} 这里是个标识符，用来识别发贴的
+    * limit {number} 限制发帖数目
+    * */
     $.limiter.incr = async function (key, limit){
-        let ret = await connection.incr(prefix + key);
+        let ret = await connection.incr(prefix + key); //redis的递增器，这个限制了只能发两个帖子? 这个是第小时只能发两个帖子
         ret = Number(ret);
         debug('incr: key=%s, counter=%s', key, ret);
         if (isNaN(ret)) return false;
