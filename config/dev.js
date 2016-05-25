de20973a2337d40855692930ad30b614e50a79ae
aloo <aloocs@gmail.com>
2016-05-25 21:14:14 +0800

    删除了配置文件

--- config/dev.js
+++ config/dev.js
@@ -2,4 +2,10 @@ module.exports = function(set, get, has) {
     set('web.port', 3001);
 
     set('db.mongodb', 'mongodb://192.168.99.100:32770/practice_nodejs_project');
-}
+
+    set('smtp.host', 'smtp.exmail.qq.com');
+    set('smtp.secure', true);
+    set('smtp.port', 465);
+    set('smtp.auth.user', 'admin@zhoup.com');
+    set('smtp.auth.pass','xx');
+};
