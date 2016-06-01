953037e038febd861c5bd90c3e8c981b68094495
aloo <aloocs@gmail.com>
2016-06-01 16:07:45 +0800

    加入了github的登录方法

--- config/dev.js
+++ config/dev.js
@@ -8,4 +8,8 @@ module.exports = function(set, get, has) {
     set('smtp.port', 465);
     set('smtp.auth.user', 'ivernaloo@163.com');
     set('smtp.auth.pass','abcd2017');
+
+    set('github.clientID', 'f584fed6fd9c0da240a1');
+    set('github.clientSecret', '6ee1c24a2e78ac85be8d944fc1d815a87c9653b3');
+    set('github.callbackURL', 'http://server:3000/auth/github');
 };
