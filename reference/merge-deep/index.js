cdc2d574c5d16c98d2e1082afe722dd2c1a50d0a
aloo <aloocs@gmail.com>
2017-02-23 18:12:55 +0800

    add array-union

--- reference/merge-deep/index.js
+++ reference/merge-deep/index.js
@@ -18,31 +18,32 @@ module.exports = function mergeDeep(orig, objects) {
   var len = arguments.length;
   var idx = 0;
 
-  while (++idx < len) {
-    var val = arguments[idx];
+  while (++idx < len) { // 游标移动
+    var val = arguments[idx]; // 对应的参数
 
     if (utils.isObject(val) || Array.isArray(val)) {
-      merge(target, val);
+      merge(target, val); // 对象或数组时拷贝
     }
   }
   return target;
 };
 
-function merge(target, obj) {
-  for (var key in obj) {
-    if (!hasOwn(obj, key)) {
+// 浅拷贝
+function merge(target, obj) {  // 把target合并到obj上
+  for (var key in obj) {  // 遍历所有的属性
+    if (!hasOwn(obj, key)) { // 检测有没有属性值
       continue;
     }
 
     var oldVal = obj[key];
     var newVal = target[key];
 
-    if (utils.isObject(newVal) && utils.isObject(oldVal)) {
-      target[key] = merge(newVal, oldVal);
+    if (utils.isObject(newVal) && utils.isObject(oldVal)) { // 现在原来的值都是对象
+      target[key] = merge(newVal, oldVal);  // 直接合并
     } else if (Array.isArray(newVal)) {
-      target[key] = utils.union([], newVal, oldVal);
+      target[key] = utils.union([], newVal, oldVal); // 数组合并?
     } else {
-      target[key] = utils.clone(oldVal);
+      target[key] = utils.clone(oldVal);  // 简单复制
     }
   }
   return target;
--- reference/merge-deep/utils.js
+++ reference/merge-deep/utils.js
@@ -8,12 +8,12 @@ var utils = require('lazy-cache')(require);
 var fn = require;
 require = utils;
 
-require('arr-union', 'union');
-require('clone-deep', 'clone');
-require('kind-of', 'typeOf');
+require('arr-union', 'union');  // union方法
+require('clone-deep', 'clone'); // 深拷贝
+require('kind-of', 'typeOf');   // 类别判断
 require = fn;
 
-utils.isObject = function(val) {
+utils.isObject = function(val) {  // 判断是否是对象
   return utils.typeOf(val) === 'object' || utils.typeOf(val) === 'function';
 };
 
