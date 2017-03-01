f1b117a92920c684844b07012bc0484ef6b84915
aloo <aloocs@gmail.com>
2017-03-01 11:46:51 +0800

    mongoose schema

--- src/init/mongodb.js
+++ src/init/mongodb.js
@@ -3,8 +3,8 @@
 import mongoose from 'mongoose';
 
 module.exports = function (done){
-    const conn = mongoose.creatConnection($.config.get('db.mongodb'))
-    $.mongodb = conn;
+    const conn = mongoose.creatConnection($.config.get('db.mongodb'));
+    $.mongodb = conn; // con 连接实例绑定到project-core上面
 
     done(); // async
 };
