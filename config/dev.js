d54c37c82610b42160fb7f1ee6e89bc747ec8993
aloo <aloocs@gmail.com>
2017-03-02 18:51:50 +0800

    update modified

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,3 @@
 module.exports = function(set, get, has){
-    set('db.mongodb', 'mongodb://127.0.0.1/practice_nodejs_project');
+    set('db.mongodb', 'mongodb://127.0.0.1:27017/practice_nodejs_project');
 };
