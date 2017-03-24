6abdf13d45edd60fbd965223a13d309a35cbf7fd
aloo <aloocs@gmail.com>
2017-03-24 15:26:41 +0800

    add topic route api

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,10 +1,34 @@
-"use strict";
+'use strict';
 
-module.exports = function(done){
 
-    $.checkLogin = function(req, res, next){
 
-    };
+module.exports = function (done) {
+
+
+  $.checkLogin = function (req, res, next) {
+
+    // check session user and user._id existed
+    if (!(req.session.user && req.session.user._id)) return next(new Error('please login firstly'));
+
+    next();
+
+  };
+
+
+  $.checkTopicAuthor = async function (req, res, next) {
+
+    const topic = await $.method('topic.get').call({_id: req.params.topic_id}); // get the topic
+    if (!topic) return next(new Error(`topic ${req.params.topic_id} does not exists`)); // throw err about the topic exist status
+
+    req.topic = topic; // assignment the topic to the request
+
+    if (req.session.user.isAdmin) return next();  // check admin status
+    if (topic.author._id.toString() === req.session.user._id.toString()) return next(); // check the author of the topic
+
+    next(new Error('access denied'));
+
+  };
+
 
     done();
 };
---
+++ src/routes/topic.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * pratice Node.js project
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+module.exports = function (done) {
+
+
+    $.router.post('/api/topic/add', $.checkLogin, async function (req, res, next) {
+
+        req.body.author = req.session.user._id;
+
+
+        // iterate the tags attribute
+        if ('tags' in req.body) {
+            req.body.tags = req.body.tags.split(',').map(v => v.trim()).filter(v => v); // tidy the tag format
+        }
+
+        const topic = await $.method('topic.add');
+
+        res.json({success: true, topic});
+        // 发布频率限制
+        // {
+        //   const key = `addtopic:${req.body.author}:${$.utils.date('YmdH')}`;
+        //   const limit = 2;
+        //   const ok = await $.limiter.incr(key, limit);
+        //   if (!ok) throw new Error('out of limit');
+        // }
+        //
+
+        //
+        // const topic = await $.method('topic.add').call(req.body);
+        //
+        // await $.method('user.incrScore').call({_id: req.body.author, score: 5});
+        //
+        // res.apiSuccess({topic});
+
+    });
+
+
+    $.router.get('/api/topic/list', async function (req, res, next) {
+
+        if ('tags' in req.query) {
+            req.query.tags = req.query.tags.split(',').map(v => v.trim()).filter(v => v);
+        }
+
+        // let page = parseInt(req.query.page, 10);
+        // if (!(page > 1)) page = 1;
+        // req.query.limit = 10;
+        // req.query.skip = (page - 1) * req.query.limit;
+
+        const list = await $.method('topic.list').call(req.query); // get topic list
+
+        // const count = await $.method('topic.count').call(req.query);
+        // const pageSize = Math.ceil(count / req.query.limit);
+
+        res.json({success: true, list});
+        // res.apiSuccess({count, page, pageSize, list});
+
+    });
+
+
+    $.router.get('/api/topic/item/:topic_id', async function (req, res, next) {
+
+        const topic = await $.method('topic.get').call({_id: req.params.topic_id});
+        if (!topic) return next(new Error(`topic ${req.params.topic_id} does not exists`));
+
+        // const userId = req.session.user && req.session.user._id && req.session.user._id.toString();
+        // const isAdmin = req.session.user && req.session.user.isAdmin;
+        //
+        // const result = {};
+        // result.topic = $.utils.cloneObject(topic);
+        // result.topic.permission = {
+        //     edit  : isAdmin || userId === result.topic.author._id,
+        //     delete: isAdmin || userId === result.topic.author._id,
+        // };
+        // result.topic.comments.forEach(item => {
+        //     item.permission = {
+        //         delete: isAdmin || userId === item.author._id,
+        //     };
+        // });
+
+        res.json({success: true, topic})
+    });
+    done();
+
+};
